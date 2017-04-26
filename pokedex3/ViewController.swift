f4f1a60f3d397749ad750dd69e70f1700a054c13
rsaeks <rsaeks@gmail.com>
2017-04-25 19:25:02 -0500

    Displaying data from CSV

--- pokedex3/ViewController.swift
+++ pokedex3/ViewController.swift
@@ -11,16 +11,41 @@ import UIKit
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     @IBOutlet weak var collection: UICollectionView!
+    
+    var pokemonArray = [Pokemon]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
             collection.dataSource = self
             collection.delegate = self
+        parsePokemonCSV()
+        
+    }
+    
+    func parsePokemonCSV() {
+        
+        let path = Bundle.main.path(forResource: "pokemon", ofType: "csv")!
+        
+        do {
+            let csv = try CSV(contentsOfURL: path)
+            let rows = csv.rows
+            //print(rows)
+            
+            for i in rows {
+                let pokeID = Int(i["id"]!)!
+                let pokeName = i["identifier"]!
+                let poke = Pokemon(name: pokeName, id: pokeID)
+                pokemonArray.append(poke)
+            }
+            
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as? PokeCell {
-                let pokemon = Pokemon(name: "Pokemon", id: indexPath.row)
+                let pokemon = pokemonArray[indexPath.row]
                 cell.configureCell(pokemon: pokemon)
                 return cell
         }
@@ -34,7 +59,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 30
+        return pokemonArray.count
     }
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
